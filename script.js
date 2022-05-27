b0a42eafde454cfb3d9aceb5fad67ac797c613d2
danielabadgamero <danielabadgamero@gmail.com>
2022-05-27 19:36:54 +0200

    Added tabs :)

---
+++ script.js
@@ -0,0 +1,47 @@
+const buttons = [...document.querySelectorAll("button")];
+const container = document.querySelector("main");
+const spanishParagraphs = [...document.getElementsByClassName("spanish")];
+const englishParagraphs = [...document.getElementsByClassName("english")];
+let lang = "spanish";
+
+function changeTab(tab) {
+  [...container.children].forEach(element => {
+    element.classList.add("hidden");
+  })
+  switch (tab) {
+    case "btn1":
+      document.querySelector("div.introduccion").classList.remove("hidden");
+      break;
+    case "btn2":
+      document.querySelector("div.desempleo").classList.remove("hidden");
+      break;
+    case "btn3":
+      document.querySelector("div.medidas").classList.remove("hidden");
+      break;
+    case "btn4":
+      document.querySelector("div.opinion").classList.remove("hidden");
+      break;
+    case "home":
+      document.querySelector("div.homepage").classList.remove("hidden");
+      break;
+    case "btn5":
+      document.querySelector("div.about").classList.remove("hidden");
+      break;
+    case "btn6":
+      document.querySelector("div.bibliografia").classList.remove("hidden");
+      break;
+    default:
+  }
+}
+
+function changeLanguage(language) {
+
+}
+
+buttons.map((button) => button.addEventListener("click", (e) => {
+  if (!e.target.id) {
+    changeTab(e.target.parentElement.id);
+  } else {
+    changeTab(e.target.id);
+  }
+}));
