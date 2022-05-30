079a105f56a7bbf4ccb5639194c687a2653a5fd1
DanielDAG <80352980+danielabadgamero@users.noreply.github.com>
2022-05-30 17:56:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function changeTab(tab) {
     container.style.paddingLeft = "200px";
     container.style.paddingRight = "700px";
     container.style.color = "black";
+    container.style.objectFit = "contain";
   } else {
     container.style.background = "hsl(228, 100%, 11%)";
     container.style.paddingLeft = "500px";
